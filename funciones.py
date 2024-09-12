7dcbc8c2b48da95006dcb55d3180f13bf9eafdf4
NadinaMaia <94121058+NadinaMaia@users.noreply.github.com>
2024-09-12 16:15:49 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,8 +1,37 @@
 import numpy as np
 
 def calcularLU(A):
-    L, U = [],[]
-    # su código
+    m=A.shape[0] #filas
+    n=A.shape[1] #columnas
+    Ac= A.copy() 
+    Ad =  A.copy() 
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return 
+
+    ## Aca calculo los valores de los multiplicadores y lo actualizo en A
+    for j in range (n): #columnas
+        pivote = Ad[j,j]
+        for i in range(1, m): #filas
+            if j < i:
+                k = calculo_k(Ad[i], pivote, j) #calculo k
+                if k != 0:
+                    Ad[i] = Ad[i] - k*Ad[j]
+                    Ac[i][j] = k #agrego k 
+                    cant_op += 1
+                else:
+                    continue
+                
+    ## Aca actualizo los valores arriba de la diagonal
+    for j in range (n): #columnas
+        for i in range(1, m): #filas
+            if j >= i:
+                Ac[i][j] = Ad[i][j]
+     
+    L = np.tril(Ac,-1) + np.eye(A.shape[0]) 
+    U = np.triu(Ac)
+    return L, U, cant_op
     
     ###########
     return L, U
