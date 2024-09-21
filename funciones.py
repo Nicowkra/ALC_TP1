625a2d2a88b657769cecaa8f4a68eb593f2ae61b
nadina <solernadina@gmail.com>
2024-09-21 17:42:57 -0300

    punto 2

--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 def calcularLU(A):
     m=A.shape[0] #filas
@@ -43,3 +44,47 @@ def inversaLU(L, U):
     y = scipy.linalg.solve_triangular(L,e, lower=True)
     x = scipy.linalg.solve_triangular(U,y)
     return Inv
+
+
+###codigo nari 
+def elim_gaussiana(A):
+    cant_op = 0
+    m=A.shape[0] #filas
+    n=A.shape[1] #columnas
+    Ac= A.copy() 
+    Ad =  A.copy() 
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return 
+
+    ## Aca calculo los valores de los multiplicadores y lo actualizo en A
+    for j in range (n): #columnas
+        pivote = Ad[j,j]
+        for i in range(1, m): #filas
+            if j < i:
+                k = calculo_k(Ad[i], pivote, j) #calculo k
+                if k != 0:
+                    Ad[i] = Ad[i] - k*Ad[j]
+                    Ac[i][j] = k #agrego k 
+                    cant_op += 1
+                else:
+                    continue
+                
+    ## Aca actualizo los valores arriba de la diagonal
+    for j in range (n): #columnas
+        for i in range(1, m): #filas
+            if j >= i:
+                Ac[i][j] = Ad[i][j]
+     
+    L = np.tril(Ac,-1) + np.eye(A.shape[0]) 
+    U = np.triu(Ac)
+    return L, U, cant_op
+    
+
+def calculo_k(fila_actual, divisor, iterador):
+    multiplicador = 0
+    if divisor != 0:
+        multiplicador = fila_actual[iterador] / divisor
+    return multiplicador
+   
